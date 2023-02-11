d86b8007d4c7feed91f8bab0350be9677465bc0f
Eyshika Agarwal <eagarwal@ieee.org>
2023-02-11 02:43:13 -0600

    added mpu

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import adafruit_bmp3xx
 import adafruit_mpu6050
 
 # i2c connection for mpu 6050
-i2c = board.I2C()
-mpu = adafruit_mpu6050.MPU6050(i2c)
+#i2c = board.I2C()
+#mpu = adafruit_mpu6050.MPU6050(i2c)
 
 # bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 
@@ -18,15 +18,21 @@ spi = board.SPI()
 cs = DigitalInOut(board.D5)
 bmp = adafruit_bmp3xx.BMP3XX_SPI(spi, cs)
 
+bmp.reset()
 bmp.pressure_oversampling = 8 #for bmp390
 bmp.temperature_oversampling = 2
+time.sleep(2)
+sea_level_feet = round(bmp.altitude * 3.28084, 2)
 
 while True:
+    height_feet = bmp.altitude * 3.28084 - sea_level_feet
     print(
-        "Pressure: {:6.4f} hPa Temperature: {:5.2f} C Altitude: {:5.4f} m".format(bmp.pressure, bmp.temperature, bmp.altitude)
+        "Pressure: {:6.4f} hPa Temperature: {:5.2f} C Altitude: {:5.4f} feet".format(bmp.pressure, bmp.temperature, height_feet)
     )
-    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
-    print("Gyro Temperature: %.2f C" % mpu.temperature)
+    #print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+    #print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+    #print("Gyro Temperature: %.2f C" % mpu.temperature)
     print("")
     time.sleep(1)
+    if height_feet < 5: #if less than 5 feet
+        print("Rocket Near Land !!")
